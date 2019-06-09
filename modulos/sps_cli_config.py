444032ea7729a8b901e63263a8bac19ed58d3ce2
Daniel Cruz <dftdcruz@gmail.com>
2019-06-09 11:46:54 -0300

    Os comandos 'getfrags' e 'sendfrag' foram reunidos no comando 'fragment'. O comando 'getnfo' foi inserido em 'sps-api'. O módulo 'py_sps_cli_base' teve partes do código extraído e compos o módulo 'sps_cli_config'.

---
+++ modulos/sps_cli_config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from subprocess import getoutput
+
+automail=True
+periodo_corrente="1º/2019"
+user_home_folder = getoutput("echo $HOME")
+app_root_folder = "/home/danielc/Documentos/Devel/GitHub/sps_fup2"
