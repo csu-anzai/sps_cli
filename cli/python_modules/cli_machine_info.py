3d8dd14f2a71cc42286d37d7b4de6bcbeeaf1166
root <root@spsfup>
2019-08-06 18:56:40 -0300

    Mudanças locais em 'sps_cli server'...

--- cli/python_modules/cli_machine_info.py
+++ cli/python_modules/cli_machine_info.py
@@ -12,16 +12,3 @@ pasta_raiz_do_aplicativo = getoutput("cli-config read RAIZ")
 pasta_de_seguranca = getoutput("cli-config read PASTA_DE_SEGURANCA")
 pasta_de_dados = getoutput("cli-config read PASTA_DE_DADOS")
 
-if hostname == "oracleVM":
-    pasta_raiz_do_aplicativo = "/home/bwb0de/Devel/sps_cli"
-
-elif hostname == "debian":
-    pasta_raiz_do_aplicativo = "/home/danielc/Documentos/Devel/GitHub/sps_fup2"
-
-elif hostname == "localhost":
-    pasta_temporaria = getoutput("echo $TMPDIR")
-    pasta_raiz_do_aplicativo = "/data/data/com.termux/files/home/sps_cli"
-
-elif hostname == "spsfup":
-    pasta_raiz_do_aplicativo = "/srv/sps_fup2"
-
